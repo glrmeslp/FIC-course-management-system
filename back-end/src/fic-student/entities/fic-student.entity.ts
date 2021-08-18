ac429d3d0de054399f5fe803fcde57f05393fd5d
Mario-Guilherme <carvalho16car@gmail.com>
2021-08-18 08:31:25 +0800

    feat(entity): create ficstudente schema

--- back-end/src/fic-student/entities/fic-student.entity.ts
+++ back-end/src/fic-student/entities/fic-student.entity.ts
@@ -1,1 +1,18 @@
-export class FicStudent {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+
+export type FicStudentDocument = FicStudent & Document;
+
+@Schema()
+export class FicStudent {
+    @Prop({ required: true })
+
+    name: string;
+
+    @Prop({ required: true })
+
+    matricula: string;
+}
+
+export  const  CatSchema  = SchemaFactory.createForClass(FicStudent);
