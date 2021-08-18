160873bd3dc0de0c62739091f4675ed7d6a6a08e
Mario-Guilherme <carvalho16car@gmail.com>
2021-08-19 01:58:13 +0800

    feat:add htppcode

--- back-end/src/fic-student/fic-student.controller.ts
+++ back-end/src/fic-student/fic-student.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
 import { FicStudentService } from './fic-student.service';
 import { CreateFicStudentDto } from './dto/create-fic-student.dto';
 import { UpdateFicStudentDto } from './dto/update-fic-student.dto';
@@ -19,16 +19,17 @@ export class FicStudentController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.ficStudentService.findOne(+id);
+    return this.ficStudentService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateFicStudentDto: UpdateFicStudentDto) {
-    return this.ficStudentService.update(+id, updateFicStudentDto);
+    return this.ficStudentService.update(id, updateFicStudentDto);
   }
 
+  @HttpCode(204)
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.ficStudentService.remove(+id);
+    return this.ficStudentService.remove(id);
   }
 }
