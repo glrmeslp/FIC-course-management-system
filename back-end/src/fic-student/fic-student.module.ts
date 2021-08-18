00d407cc6d2019813e0379bd8e55f4849f6421f5
Mario-Guilherme <carvalho16car@gmail.com>
2021-08-19 01:42:09 +0800

    feat:import ficStudentSchema and mongoosemodule

--- back-end/src/fic-student/fic-student.module.ts
+++ back-end/src/fic-student/fic-student.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FicStudentService } from './fic-student.service';
 import { FicStudentController } from './fic-student.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FicStudent, FicStudentSchema } from './entities/fic-student.entity';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: FicStudent.name, schema: FicStudentSchema}])],
   controllers: [FicStudentController],
   providers: [FicStudentService]
 })
