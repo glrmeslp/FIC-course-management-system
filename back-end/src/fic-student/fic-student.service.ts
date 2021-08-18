dd5e3e5c1acdcc862a084cf03fb8fe8cfa9708c1
Mario-Guilherme <carvalho16car@gmail.com>
2021-08-19 01:40:13 +0800

    feat(service):create crud

--- back-end/src/fic-student/fic-student.service.ts
+++ back-end/src/fic-student/fic-student.service.ts
@@ -1,26 +1,33 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { CreateFicStudentDto } from './dto/create-fic-student.dto';
 import { UpdateFicStudentDto } from './dto/update-fic-student.dto';
+import { FicStudent, FicStudentDocument } from './entities/fic-student.entity';
 
 @Injectable()
 export class FicStudentService {
-  create(createFicStudentDto: CreateFicStudentDto) {
-    return 'This action adds a new ficStudent';
+
+  constructor(@InjectModel(FicStudent.name) private ficStudentModel: Model<FicStudentDocument>) {}
+
+  async create(createFicStudentDto: CreateFicStudentDto): Promise<FicStudent> {
+    const createdficStudent = new this.ficStudentModel(createFicStudentDto);
+    return createdficStudent.save();
   }
 
-  findAll() {
-    return `This action returns all ficStudent`;
+  async findAll(): Promise<FicStudent[]> {
+    return  this.ficStudentModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} ficStudent`;
+  async findOne(id: string):Promise<FicStudent> {
+    return  this.ficStudentModel.findById(id).exec();
   }
 
-  update(id: number, updateFicStudentDto: UpdateFicStudentDto) {
-    return `This action updates a #${id} ficStudent`;
+  async update(id: string, updateFicStudentDto: UpdateFicStudentDto) {
+    return  this.ficStudentModel.findByIdAndUpdate(id, updateFicStudentDto).exec();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} ficStudent`;
+  async remove(id: string): Promise<FicStudent> {
+    return  this.ficStudentModel.findByIdAndDelete(id).exec();
   }
 }
